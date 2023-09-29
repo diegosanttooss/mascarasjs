c2e81538e47ad77ee796b3401ab7c198bb869f0a
diegosanttooss <146029122+diegosanttooss@users.noreply.github.com>
2023-09-29 14:18:01 -0300

    upload dos arquivos da mascara cpf e cnpj

---
+++ cpf.js
@@ -0,0 +1,67 @@
+
+
+
+function  cpf ()
+
+{
+
+
+    const  input = document.querySelector("input")
+
+    input.addEventListener("keypress", () => {
+    
+    let inputlength = input.value.length 
+    if 
+    ( inputlength == 3 || inputlength==7) {
+        input.value+="."
+    }
+    
+    else if (inputlength==11) {
+        input.value += "-"
+    }
+    
+    }
+    )
+
+    
+}
+
+
+// query selector: tag, id, classe
+//  para id e classe utilizar a mesma 
+// 
+
+
+
+
+{
+
+
+    const  cnpj = document.querySelector(".cnpj")
+
+    cnpj.addEventListener("keypress", () => {
+    
+    let cnpjlength = cnpj.value.length 
+    if 
+    ( cnpjlength == 2 || cnpjlength==6) {
+        cnpj.value+="."
+    }
+    
+    else if (cnpjlength==10) {
+        cnpj.value += "/"
+    }
+    else if (cnpjlength == 15) {
+
+        cnpj.value += "-"
+    }
+    }
+    )
+
+
+
+
+
+
+
+
+}
